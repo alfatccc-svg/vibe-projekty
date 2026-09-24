a4a40fe4e5228282d8d3e17daf82f82447ec3ce6
Jan62 <alfatccc@tccc-cz.org>
2026-09-24 12:32:37 +0200

    Nový projekt: Napis_Python_skript_ktery_spocita_soucet_2026-09-24_06-32

---
+++ projekty/Napis_Python_skript_ktery_spocita_soucet_2026-09-24_06-32.py
@@ -0,0 +1,8 @@
+# Program pro výpočet součtu čísel od 1 do 100
+
+# Výpočet pomocí vestavěné funkce sum() a rozsahu range(1, 101)
+# range(1, 101) generuje čísla od 1 do 100 (horní meze se neúčastní)
+soucet = sum(range(1, 101))
+
+# Výpis výsledku do konzole
+print(f"Součet čísel od 1 do 100 je: {soucet}")
